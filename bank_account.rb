0efe57d4bd251f7bf8721e1a547833bdee13557e
rodrigo <rob.developer@protonmail.com>
2021-07-01 14:24:44 -0400

    add: constructor methods

--- bank_account.rb
+++ bank_account.rb
@@ -2,15 +2,30 @@
 #Ejercicio 1
 
 class User
-    @name = name
 
+    def initialize(name)
+        @name = name
+    end
+    
     def total_balance()
+    end
+
+    def transfer(ammount, bank_account)
+        
     end
 end
 
 class BankAccount
-    @bank_name = bank_name
-    @number_account = number_account
-    @balance = balance
-
+    attr_reader :bank_name, :number_account, :balance
+    
+    def initialize(bank_name, number_account, balance = 0)
+        @bank_name = bank_name
+        @number_account = number_account
+        @balance = balance
+    end
 end
+
+account1 = BankAccount.new('Banco Estado', 987, 5000)
+account2 = BankAccount.new('Santander', 456)
+
+puts account1, account2, account1.balance, account2.balance
