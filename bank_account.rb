955b9feec6af9354402479eb1bcb2c76316ad22f
rodrigo <rob.developer@protonmail.com>
2021-07-01 14:13:09 -0400

    initial commit

---
+++ bank_account.rb
@@ -0,0 +1,16 @@
+#Desafío cuentas bancarias y barajas de cartas
+#Ejercicio 1
+
+class User
+    @name = name
+
+    def total_balance()
+    end
+end
+
+class BankAccount
+    @bank_name = bank_name
+    @number_account = number_account
+    @balance = balance
+
+end
