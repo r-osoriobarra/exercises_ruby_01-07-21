79744c51a150b1aef9f771d20c458eb37a9e22ee
rodrigo <rob.developer@protonmail.com>
2021-07-03 00:16:35 -0400

    order methods in cards.rb

--- card.rb
+++ card.rb
@@ -1,14 +1,19 @@
 #Desafío Cuenta Bancaria - Ejercicio 1
 class Card
     attr_accessor :number, :kind
-
     def initialize(number, kind)
-        if (["C","D","E","T"].include? kind) && ((1..13).include? number)
-            @number = number
-            @kind = kind
-        else
-            raise "number o kind no es un argumento válido" 
-        end
+        validate_number(number)
+        validate_kind(kind)
+    end
+
+    def validate_number(number)
+        raise "Número no válido [1,13]" if ((1..13).include? number) == false
+        @number = number
+    end
+
+    def validate_kind(kind)
+        raise "Pinta no válida [C, D, E, T]" if (%w[C D E T].include? kind) == false
+        @kind = kind
     end
 
     def to_s
