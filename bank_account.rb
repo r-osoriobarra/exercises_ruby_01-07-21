c676329d432e9b0208e64be728f152ed29f0da7b
rodrigo <rob.developer@protonmail.com>
2021-07-02 20:05:42 -0400

    add: changes on bank_account.rb

--- bank_account.rb
+++ bank_account.rb
@@ -1,58 +1,49 @@
 #Desafío cuentas bancarias y barajas de cartas
 #Ejercicio 1
 
-class User
-    attr_reader :name, :bank_account
-
-    def initialize(name, bank_account)
-        @name = name
-        valid_array(bank_account)
-    end
-
-    def valid_array(bank_account)
-        if bank_account.class != Array and bank_account.empty? 
-            raise "Se require un array con al menos una cuenta bancaria"
-        else
-            @bank_account = bank_account        
-        end
-    end
-
-    def total_balance()
-        total_accounts = 0 
-
-        self.bank_account.each do |i|
-            total_accounts += i.balance
-        end
-
-        return total_accounts
-    end
-end
-
 class BankAccount
-    attr_reader :bank_name, :number_account, :ammount, :bank_account
+    attr_reader :bank_name, :number_account, :ammount, :transfer_account
     attr_accessor :balance
-    
-    def initialize(bank_name, number_account, balance = 0, ammount = 'nil', bank_account = 'nil')
+    def initialize(bank_name, number_account, balance = 0)
         @bank_name = bank_name
         @number_account = number_account
         @balance = balance
-        @ammount = ammount
-        @bank_account = bank_account
     end
 
-    def transfer(ammount, bank_account)
-        
+    def transfer(ammount, transfer_account)
+        valid_transfer(ammount)
+        self.balance -= self.ammount
+        transfer_account.balance += self.ammount
+    end
+
+    def valid_transfer(ammount)
         raise "Saldo insuficiente" if ammount > self.balance 
-        self.balance -= ammount
-        bank_account.balance += ammount
+        @ammount = ammount
     end
 end
 
-account1 = BankAccount.new('Banco Estado', 987, 5000)
-account2 = BankAccount.new('Santander', 456, 5000)
-account3 = BankAccount.new('Santander', 459, 1500)
-account1.transfer(5000, account2)
-#puts account1.balance, account2.balance
+class User
+    attr_reader :user_name, :bank_accounts
+    def initialize(user_name, bank_accounts)
+        @user_name = user_name
+        valid_array(bank_accounts)
+    end
 
-user1 = User.new('user_name',[account1, account3])
-#puts user1.total_balance
+    def total_balance
+        total = 0 
+        self.bank_accounts.each {|i| total += i.balance}
+        return total
+    end
+
+    def valid_array(bank_accounts)
+        if bank_accounts.class != Array and bank_accounts.empty? 
+            raise "Se require un array con al menos una cuenta bancaria" 
+        end
+        @bank_accounts = bank_accounts        
+    end
+end
+
+#account1 = BankAccount.new('Banco Estado', 987, 5000)
+#account2 = BankAccount.new('Santander', 456, 5000)
+#account1.transfer(5000, account2)
+#puts account1.balance, account2.balance
